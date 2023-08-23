be2ab613b8d3470839fcf5caa2b916aadcda049b
Jade Stewart <jadekstewart3@gmail.com>
2023-08-23 12:43:19 -0600

    test park service

---
+++ spec/services/park_service_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+RSpec.describe ParkService do 
+  describe "class methods" do
+    context "#connection" do
+      it "returns a json object" do
+        parks = ParkService.new.get_state_parks("TN")
+        expect(parks).to be_a(Hash)
+        expect(parks.keys).to eq([:total, :limit, :start, :data])
+
+        expect(parks[:data]).to be_an(Array)
+      end
+    end
+  end
+end
